42ca3005c14d2d976517055daadfbc75de66287e
Nicolas Cherel <nicolas.cherel@gmail.com>
2014-12-21 13:48:32 +0100

    clean up basic test

--- src/lib.rs
+++ src/lib.rs
@@ -150,35 +150,6 @@ rustlex! HBExpressionLexer {
 
 }
 
-
-#[test]
-fn it_works() {
-  println!("{}", parse(r##"
-    conten content
-    {{pouet.so1}}
-    {{#pouet2}} do do do {{/pouet2}}
-    {{#pouet3}} do do do {{/pouet3 }}
-    {{#deep}}
-      zero
-      {{#deep1}}
-        one
-        {{#deep2}}
-          two
-          {{#deep3}}
-            bottom 3
-            {{at.level.3}}
-          {{/deep3}}
-        {{/deep2}}
-        {{level1}}
-      {{/deep1}}
-    {{/deep}}
-    {{{toto }}}
-    {{{toto2 coyote=speed.runner hello=how tip="top"}}}
-    {{{toto3.[3].[#jojo] titi="grominet"}}}
-    {{t "… param1" well.[that my baby].[1] ~}}
-  "##))
-}
-
 #[deriving(Show)]
 enum HBValHolder {
   String(String),
