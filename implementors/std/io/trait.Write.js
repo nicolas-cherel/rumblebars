32ef70c8f9d483be395b180a29b751cf6d024816
Nicolas Cherel <nicolas.cherel@gmail.com>
2015-07-07 17:56:00 +0200

    docs updates

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rumblebars'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='enum' href='rumblebars/enum.SafeWriting.html' title='rumblebars::SafeWriting'>SafeWriting</a>&lt;'a&gt;",];
+implementors['rumblebars'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='enum' href='rumblebars/enum.SafeWriting.html' title='rumblebars::SafeWriting'>SafeWriting</a>&lt;'a&gt;","impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='rumblebars/struct.HTMLSafeWriter.html' title='rumblebars::HTMLSafeWriter'>HTMLSafeWriter</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
